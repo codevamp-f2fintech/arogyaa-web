49abaee0501e67be1edaf0b1d70d6a4090e5e5e2
monismohsin <monismohsin1998@gmail.com>
2024-08-13 10:03:12 +0530

    implemented sign up page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,133 @@
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+} from "@mui/material";
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import GoogleIcon from "@mui/icons-material/Google";
+import Image from "next/image";
+
+const signup = () => {
+  return (
+    <>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+          marginTop: "4",
+          marginRight: "4",
+        }}
+      >
+        <Box
+          sx={{
+            flex: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            src="/image/A1.jpg"
+            alt="doctor"
+            width={750}
+            height={500}
+            layout="resposive"
+            style={{}}
+          />
+        </Box>
+        <Box
+          sx={{
+            flex: 1,
+            maxWidth: 400,
+            padding: 4,
+            margin: 2,
+            marginBottom: "8",
+            alignContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{ fontWeight: "bold", fontSize: "h6.fontSize", padding: 4 }}
+          >
+            Welcome To Arogyaa
+          </Typography>
+
+          <Typography variant="h6" sx={{ fontSize: "default" }}>
+            Please Sign-Up To Your Account For Consult With Our Doctors{" "}
+          </Typography>
+
+          <Box component="form" noValidate autoComplete="off">
+            <TextField
+              style={{ marginTop: 10 }}
+              fullWidth
+              id="outlined-basic"
+              label="Full Name"
+              variant="outlined"
+            />
+            <TextField
+              style={{ marginTop: 10 }}
+              fullWidth
+              id="outlined-basic"
+              label="Email Address"
+              variant="outlined"
+            />
+            <TextField
+              style={{ marginTop: 10 }}
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+            <TextField
+              style={{ marginTop: 10 }}
+              fullWidth
+              id="outlined-basic"
+              label="Confirm-Password"
+              variant="outlined"
+            />
+          </Box>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="remind me"
+            sx={{ display: "block", textAlign: "left" }}
+          />
+          <Button fullWidth variant="contained">
+            Sign Up
+          </Button>
+          <Box>
+            <Button>FORGET PASSWORD ?</Button>
+            <Button>CREATE AN ACCOUNT</Button>
+          </Box>
+          <Typography variant="subtitle1">or sign in with </Typography>
+          <Box>
+            <Button>
+              <FacebookIcon />
+            </Button>
+            <Button>
+              <XIcon />
+            </Button>
+            <Button>
+              <GitHubIcon />
+            </Button>
+            <Button>
+              <GoogleIcon />
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default signup;
