b25021dca9548235f4a3c83a87d7d9b7c88dffbe
coder-faraz <hfaraz316@gmail.com>
2024-08-21 22:27:39 +0530

    complete api setup with folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-
 'use client';
-import { useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from '@/redux/store';
-import { increment } from '@/redux/features/userSlice';
 
 const App = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const { user, loading } = useSelector((state: RootState) => state.user);
-
   return (
     <main>
-      <div>
-        Loading is {loading.toString()}
-      </div>
-      <button
-        onClick={() => dispatch(increment())}
-      > Change </button>
+      Hello Next.js
     </main>
   );
 };
