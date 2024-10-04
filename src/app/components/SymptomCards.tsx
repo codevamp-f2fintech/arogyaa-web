6f964d50bead83d575b41c7af4967d4a28ffcb90
monismohsin <monismohsin1998@gmail.com>
2024-10-04 17:12:30 +0530

    add some changes

--- src/app/components/SymptomCards.tsx
+++ src/app/components/SymptomCards.tsx
@@ -1,103 +1,120 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import { Box, Button, CardMedia, CardContent, Grid, Paper, Typography } from '@mui/material';
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import {
+  Box,
+  Button,
+  CardMedia,
+  CardContent,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
 
-import en from '@/locales/en.json';
-import styles from '../page.module.css';
+import en from "@/locales/en.json";
+import styles from "../page.module.css";
 
-import type { AppDispatch, RootState } from '@/redux/store';
+import type { AppDispatch, RootState } from "@/redux/store";
 import { icons } from "@/static-data";
-import { setSymptoms, setLoading } from '@/redux/features/symptomsSlice';
-import { useGetSymptoms } from '@/hooks/symptoms';
+import { setSymptoms, setLoading } from "@/redux/features/symptomsSlice";
+import { useGetSymptoms } from "@/hooks/symptoms";
 
 const SymptomCards: React.FC = () => {
-    const dispatch: AppDispatch = useDispatch();
-    const { symptoms } = useSelector((state: RootState) => state.symptoms);
+  const dispatch: AppDispatch = useDispatch();
+  const { symptoms } = useSelector((state: RootState) => state.symptoms);
 
-    const [pageSize, setPageSize] = useState({
-        page: 1,
-        size: 8
-    });
+  const [pageSize, setPageSize] = useState({
+    page: 1,
+    size: 8,
+  });
 
-    // Fetch data from the API
-    const { data } = useGetSymptoms([], `http://localhost:3001/api/symptoms/get-symptoms`);
+  // Fetch data from the API
+  const { data } = useGetSymptoms(
+    [],
+    `http://localhost:3001/api/symptoms/get-symptoms`
+  );
 
-    useEffect(() => {
-        if (data && data.length > 0) {
-            dispatch(setSymptoms(data));
-        }
-    }, [data]);
+  useEffect(() => {
+    if (data && data.length > 0) {
+      dispatch(setSymptoms(data));
+    }
+  }, [data]);
 
-    const handleFetchNext = () => {
-        setPageSize(prevSize => ({
-            ...prevSize,
-            size: prevSize.size + 5,
-        }));
-        dispatch(setLoading(true));
-    };
+  const handleFetchNext = () => {
+    setPageSize((prevSize) => ({
+      ...prevSize,
+      size: prevSize.size + 5,
+    }));
+    dispatch(setLoading(true));
+  };
 
-    // Display data logic
-    const displayData = symptoms.length > 0 ? symptoms : [];
+  // Display data logic
+  const displayData = symptoms.length > 0 ? symptoms : [];
 
-    return (
-        <Box className={styles.symptomBox}>
-            <Typography variant="h2" component="h2" className={styles.symptomTitle}>
-                <span className={styles.symptomTitleSpan}>{en.homepage.symptomCards.title1}</span>
-                <br />
-                {en.homepage.symptomCards.title2}
-            </Typography>
+  return (
+    <Box className={styles.symptomBox}>
+      <Typography variant="h2" component="h2" className={styles.symptomTitle}>
+        <span className={styles.symptomTitleSpan}>
+          {en.homepage.symptomCards.title1}
+        </span>
+        <br />
+        {en.homepage.symptomCards.title2}
+      </Typography>
 
-            <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {displayData.slice(0, pageSize.size).map((item) => {
-                    const icon = icons.find((icon) => icon.title === item.name)?.path;
+      <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {displayData.slice(0, pageSize.size).map((item) => {
+          const icon = icons.find((icon) => icon.title === item.name)?.path;
 
-                    return (
-                        <Grid item xs={3} className={styles.symptomGrid} key={item.id}>
-                            <Paper elevation={3} className={styles.symptomPaper}>
-                                <CardMedia
-                                    component="img"
-                                    className={styles.symptomCardMedia}
-                                    image={icon}
-                                    alt={icon}
-                                />
-                                <CardContent sx={{ textAlign: 'center' }}>
-                                    <Typography variant="h5" component="h5" className={styles.symptomCardTitle}>
-                                        {item.name}
-                                    </Typography>
+          return (
+            <Grid item xs={3} className={styles.symptomGrid} key={item.id}>
+              <Paper elevation={3} className={styles.symptomPaper}>
+                <CardMedia
+                  component="img"
+                  className={styles.symptomCardMedia}
+                  image={icon}
+                  alt={icon}
+                />
+                <CardContent sx={{ textAlign: "center" }}>
+                  <Typography
+                    variant="h5"
+                    component="h5"
+                    className={styles.symptomCardTitle}
+                  >
+                    {item.name}
+                  </Typography>
 
-                                    <Button
-                                        variant="contained"
-                                        className={styles.symptomButton}
-                                        endIcon={<ArrowCircleRightIcon />}
-                                    >
-                                        Consult Now
-                                    </Button>
-                                </CardContent>
-                            </Paper>
-                        </Grid>
-                    )
-                })}
+                  <Button
+                    variant="contained"
+                    className={styles.symptomButton}
+                    endIcon={<ArrowCircleRightIcon />}
+                  >
+                    Consult Now
+                  </Button>
+                </CardContent>
+              </Paper>
             </Grid>
+          );
+        })}
+      </Grid>
 
-            <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12 }}>
-                <Grid xs={12} sx={{ textAlign: 'center', marginTop: '20px' }}>
-                    <Button
-                        variant="contained"
-                        className={styles.gridButton}
-                        endIcon={<ArrowCircleRightIcon />}
-                        onClick={handleFetchNext}
-                    >
-                        {en.homepage.symptomCards.buttonText}
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+      <Grid container spacing={2} columns={{ xs: 12, sm: 12, md: 12 }}>
+        <Grid xs={12} sx={{ textAlign: "center", marginTop: "20px" }}>
+          <Button
+            variant="contained"
+            className={styles.gridButton}
+            endIcon={<ArrowCircleRightIcon />}
+            onClick={handleFetchNext}
+          >
+            {en.homepage.symptomCards.buttonText}
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default SymptomCards;
--- src/app/components/common/Topbar.tsx
+++ src/app/components/common/Topbar.tsx
@@ -1,33 +1,33 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Popover from '@mui/material/Popover';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Popover from "@mui/material/Popover";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 import en from "@/locales/en.json";
 import styles from "../../page.module.css";
 
-import { AppDispatch, RootState } from '@/redux/store';
-import { useGetNotifications } from '@/hooks/notification';
-import { setNotifications } from '@/redux/features/notificationsSlice';
+import { AppDispatch, RootState } from "@/redux/store";
+import { useGetNotifications } from "@/hooks/notification";
+import { setNotifications } from "@/redux/features/notificationsSlice";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -40,21 +40,23 @@ const Topbar = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
   const dispatch: AppDispatch = useDispatch();
-  const { notifications } = useSelector((state: RootState) => state.notifications);
+  const { notifications } = useSelector(
+    (state: RootState) => state.notifications
+  );
 
   //fetch api
-  const { data } = useGetNotifications([], `http://localhost:3001/api/notifications/get-notifications/60d9caed6f70c40b7cdcb867`);
+  const { data } = useGetNotifications(
+    [],
+    `http://localhost:3001/api/notifications/get-notifications/60d9caed6f70c40b7cdcb867`
+  );
 
-  console.log('data:', data); // Inspect the structure of the response
   let dataArray = Array.isArray(data) ? data : [data];
 
   useEffect(() => {
     if (dataArray && dataArray.length > 0) {
       dispatch(setNotifications(dataArray));
-      console.log('working')
     }
   }, [data, dispatch]);
-  console.log(notifications);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -95,20 +97,22 @@ const Topbar = () => {
   };
 
   const handleViewMore = () => {
-    setVisibleNotifications(prev => prev + 5);
+    setVisibleNotifications((prev) => prev + 5);
   };
 
   function setPageSize(arg0: (prevSize: any) => any) {
-    throw new Error('Function not implemented.');
+    throw new Error("Function not implemented.");
   }
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <AppBar className={styles.appBar}>
       <Toolbar disableGutters>
-        <AdbIcon sx={{ display: { xs: 'none', md: 'flex', color: '#000' }, mr: 1 }} />
+        <AdbIcon
+          sx={{ display: { xs: "none", md: "flex", color: "#000" }, mr: 1 }}
+        />
         <Typography
           variant="h6"
           noWrap
@@ -116,18 +120,18 @@ const Topbar = () => {
           href="#app-bar-with-responsive-menu"
           sx={{
             mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
             fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: '#000',
-            textDecoration: 'none',
+            letterSpacing: ".3rem",
+            color: "#000",
+            textDecoration: "none",
           }}
         >
           {en.topbar.title}
         </Typography>
 
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', } }}>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
           <IconButton
             size="large"
             aria-label="account of current user"
@@ -153,19 +157,17 @@ const Topbar = () => {
             open={Boolean(anchorElNav)}
             onClose={handleCloseNavMenu}
             sx={{
-              display: { xs: 'block', md: 'none', justifyContent: 'center' },
+              display: { xs: "block", md: "none", justifyContent: "center" },
             }}
           >
             {pages.map((page) => (
               <MenuItem key={page} onClick={handleCloseNavMenu}>
-                <Typography className={styles.menuItem}>
-                  {page}
-                </Typography>
+                <Typography className={styles.menuItem}>{page}</Typography>
               </MenuItem>
             ))}
           </Menu>
         </Box>
-        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+        <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
         <Typography
           variant="h5"
           noWrap
@@ -173,19 +175,24 @@ const Topbar = () => {
           href="#app-bar-with-responsive-menu"
           sx={{
             mr: 2,
-            display: { xs: 'flex', md: 'none' },
+            display: { xs: "flex", md: "none" },
             flexGrow: 1,
-            fontFamily: 'monospace',
+            fontFamily: "monospace",
             fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
           }}
         >
           LOGO
         </Typography>
 
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'center' } }}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex", justifyContent: "center" },
+          }}
+        >
           {pages.map((page) => (
             <Button
               key={page}
@@ -208,13 +215,22 @@ const Topbar = () => {
         </Box>
 
         <Box>
-          <IconButton aria-describedby={id} onClick={handleClick} sx={{
-            color: '#000', backgroundColor: '#FAFAFA', padding: '10px', borderRadius: '500px', margin: '0 10px 0 5px', ':hover': {
-              bgcolor: '#20ADA0',
-              color: 'white',
-            },
-          }}>
-            <NotificationsIcon sx={{ color: 'black' }} />
+          <IconButton
+            aria-describedby={id}
+            onClick={handleClick}
+            sx={{
+              color: "#000",
+              backgroundColor: "#FAFAFA",
+              padding: "10px",
+              borderRadius: "500px",
+              margin: "0 10px 0 5px",
+              ":hover": {
+                bgcolor: "#20ADA0",
+                color: "white",
+              },
+            }}
+          >
+            <NotificationsIcon sx={{ color: "black" }} />
           </IconButton>
           <Popover
             anchorReference="anchorPosition"
@@ -224,13 +240,19 @@ const Topbar = () => {
             anchorEl={anchorEl}
             onClose={handleClose}
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
+              vertical: "bottom",
+              horizontal: "left",
             }}
-            sx={{ borderRadius: 'px' }}
+            sx={{ borderRadius: "px" }}
           >
             <Box sx={{ width: 400 }}>
-              <Tabs value={tabValue} onChange={handleTabChange} variant="fullWidth" textColor="#20ADA0" indicatorColor="black">
+              <Tabs
+                value={tabValue}
+                onChange={handleTabChange}
+                variant="fullWidth"
+                textColor="#20ADA0"
+                indicatorColor="black"
+              >
                 <Tab label="Unread" sx={{ color: "#20ADA0" }} />
                 <Tab label="Read" sx={{ color: "#20ADA0" }} />
               </Tabs>
@@ -240,26 +262,33 @@ const Topbar = () => {
                     {notifications.length === 0 ? (
                       <Typography>No unread Notifications</Typography>
                     ) : (
-                      notifications.slice(0, visibleNotifications).map((notification, index) => (
-                        <Typography key={index}>
-                          {notification.message}
-                          <IconButton onClick={() => markAsRead(index)}>
-                            <MarkEmailReadIcon sx={{ color: "#20ADA0" }} />
-                          </IconButton>
-                          {notification.message}
-                          <IconButton onClick={() => markAsRead(index)}>
-                            <MarkEmailReadIcon sx={{ color: "#20ADA0" }} />
-                          </IconButton>
-                        </Typography>
-                      ))
+                      notifications
+                        .slice(0, visibleNotifications)
+                        .map((notification, index) => (
+                          <Typography key={index}>
+                            {notification.message}
+                            <IconButton onClick={() => markAsRead(index)}>
+                              <MarkEmailReadIcon sx={{ color: "#20ADA0" }} />
+                            </IconButton>
+                            {notification.message}
+                            <IconButton onClick={() => markAsRead(index)}>
+                              <MarkEmailReadIcon sx={{ color: "#20ADA0" }} />
+                            </IconButton>
+                          </Typography>
+                        ))
                     )}
                     {notifications.length > visibleNotifications && (
-                      <Button onClick={handleViewMore} sx={{
-                        color: "black",
-                        ":hover": {
-                          bgcolor: "#20ADA0"
-                        }
-                      }}>View More</Button>
+                      <Button
+                        onClick={handleViewMore}
+                        sx={{
+                          color: "black",
+                          ":hover": {
+                            bgcolor: "#20ADA0",
+                          },
+                        }}
+                      >
+                        View More
+                      </Button>
                     )}
                   </Box>
                 ) : (
@@ -267,11 +296,11 @@ const Topbar = () => {
                     {readNotification.length === 0 ? (
                       <Typography>No read notifications.</Typography>
                     ) : (
-                      readNotification.slice(0, visibleNotifications).map((notification, index) => (
-                        <Typography key={index}>
-                          {notification}
-                        </Typography>
-                      ))
+                      readNotification
+                        .slice(0, visibleNotifications)
+                        .map((notification, index) => (
+                          <Typography key={index}>{notification}</Typography>
+                        ))
                     )}
                     {readNotification.length > visibleNotifications && (
                       <Button onClick={handleViewMore}>View More</Button>
