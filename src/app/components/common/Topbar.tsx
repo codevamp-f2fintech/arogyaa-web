0d0d12be7a05a6726769d7ca8a30f7e30b4717f4
coder-faraz <hfaraz316@gmail.com>
2025-01-06 16:58:52 +0530

    code optimization 2

--- src/app/components/common/Topbar.tsx
+++ src/app/components/common/Topbar.tsx
@@ -125,7 +125,7 @@ const Topbar = () => {
           variant="h6"
           noWrap
           component="a"
-          href="#app-bar-with-responsive-menu"
+          href="/"
           sx={{
             mr: 2,
             display: { xs: "none", md: "flex" },
