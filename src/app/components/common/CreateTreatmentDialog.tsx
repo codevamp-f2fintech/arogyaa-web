305134da8e53ad96be39fb0d8e78c14dffe962e7
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2025-02-10 11:25:58 +0530

    treatment-historyandmodal

--- src/app/components/common/CreateTreatmentDialog.tsx
+++ src/app/components/common/CreateTreatmentDialog.tsx
@@ -16,21 +16,20 @@ import {
   TextField,
 } from "@mui/material";
 import {
-  AddCircle,
+  Image as ImageIcon,
   Close,
   CheckCircle,
-  Description,
-  Timer,
-  Repeat,
-  Inventory,
   ListAlt,
+  Description,
+  Inventory,
+  Repeat,
+  Timer,
 } from "@mui/icons-material";
 import { styled } from "@mui/system";
 import { creator } from "@/apis/apiClient";
 import { Utility } from "@/utils";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/redux/store";
-
 
 const StyledTextField = styled(TextField)({
   "& label": {
@@ -51,6 +50,7 @@ const StyledTextField = styled(TextField)({
     },
   },
 });
+
 const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
@@ -80,10 +80,11 @@ const optionsStatus = [
   { label: "In Progress", value: "in progress" },
   { label: "Completed", value: "completed" },
 ];
+
 interface CreateTreatmentDialogProps {
   open: boolean;
   onClose: () => void;
-  fetchTreatments: () => void;
+  fetchTreatments: (newTreatment: any) => void;
 }
 
 const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
@@ -94,7 +95,11 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
   const dispatch: AppDispatch = useDispatch();
   const { snackbarAndNavigate, decodedToken } = Utility();
   const patientId = decodedToken()?.id;
-
+  const handleClose = (event: any, reason: string) => {
+    if (reason !== "backdropClick") {
+      onClose();
+    }
+  };
   const [formData, setFormData] = useState({
     name: "",
     description: "",
@@ -102,49 +107,75 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
     frequency: "",
     duration: "",
     isEmptyStomach: false,
-    type: "arogyaa",
+    type: "",
     status: "in progress",
+    photo: null,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    quantity: "",
+    frequency: "",
+    duration: "",
+    type: "",
+  });
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = {
+      name: formData.name ? "" : "Name is required",
+      description: formData.description ? "" : "Description is required",
+      quantity: formData.quantity ? "" : "Quantity is required",
+      frequency: formData.frequency ? "" : "Frequency is required",
+      duration: formData.duration ? "" : "Duration is required",
+      type: formData.type ? "" : "Type is required",
+    };
+
+    setErrors(newErrors);
+    valid = !Object.values(newErrors).some((error) => error !== "");
+    return valid;
+  };
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFormData((prevData) => ({
+        ...prevData,
+        photo: file,
+      }));
+    }
   };
 
   const handleCheckboxChange = (e) => {
     setFormData({ ...formData, isEmptyStomach: e.target.checked });
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const handleSubmit = async () => {
+    if (!validateForm()) return;
     try {
-      const response = await creator("treatment", "create-treatment", {
-        ...formData,
-        patientId,
-      });
-
-      if (!response) throw new Error("Failed to create treatment");
-
-      snackbarAndNavigate(
-        dispatch,
-        true,
-        "success",
-        "Treatment created successfully"
+      const headers = {
+        "Content-Type": "multipart/form-data",
+      };
+      const response = await creator(
+        "treatment",
+        "create-treatment",
+        {
+          ...formData,
+          patientId,
+        },
+        headers
       );
-
-      // **Reset Form Fields after successful submission**
-      setFormData({
-        name: "",
-        description: "",
-        quantity: "",
-        frequency: "",
-        duration: "",
-        isEmptyStomach: false,
-        type: "arogyaa",
-        status: "in progress",
-      });
-
-      fetchTreatments();
-      onClose();
+      if (response.statusCode == 201) {
+        snackbarAndNavigate(dispatch, true, "success", " Created successfully");
+        fetchTreatments(response.data);
+        onClose();
+      }
     } catch (error) {
       snackbarAndNavigate(
         dispatch,
@@ -158,17 +189,13 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
   return (
     <Dialog
       open={open}
-      onClose={onClose}
+      onClose={handleClose}
       fullWidth
       maxWidth="lg"
       sx={{ borderRadius: "50px" }}
     >
       <Box sx={{ backgroundColor: "#f5f5f5", borderRadius: 4, padding: 2 }}>
-        <DialogTitle
-          sx={{
-            color: "#20ADA0",
-          }}
-        >
+        <DialogTitle sx={{ color: "#20ADA0" }}>
           <Typography variant="h6">Create Treatment</Typography>
           <IconButton
             onClick={onClose}
@@ -188,14 +215,17 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
               <StyledTextField
                 label="Name"
                 name="name"
+              
                 fullWidth
                 margin="dense"
                 value={formData.name}
                 onChange={handleChange}
+                error={!!errors.name}
+                helperText={errors.name}
                 required
-                placeholder="Enter treatment name"
+                placeholder="Enter treatment name(e.g.,paracetamol)"
                 InputProps={{
-                  startAdornment: <ListAlt sx={{ color: "#20ADA0" }} />,
+                  startAdornment: <ListAlt sx={{ color: "#20ADA0", mr: 2 }} />,
                 }}
               />
             </Grid>
@@ -207,9 +237,13 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                 margin="dense"
                 value={formData.description}
                 onChange={handleChange}
-                placeholder="Enter description"
+                error={!!errors.description}
+                helperText={errors.description}
+                placeholder="Enter description(e.g.,For fever)"
                 InputProps={{
-                  startAdornment: <Description sx={{ color: "#20ADA0" }} />,
+                  startAdornment: (
+                    <Description sx={{ color: "#20ADA0", mr: 2 }} />
+                  ),
                 }}
               />
             </Grid>
@@ -217,13 +251,18 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
               <StyledTextField
                 label="Quantity"
                 name="quantity"
+                
                 fullWidth
                 margin="dense"
                 value={formData.quantity}
                 onChange={handleChange}
-                placeholder="Enter quantity"
+                error={!!errors.quantity}
+                helperText={errors.quantity}
+                placeholder="Enter quantity(e.g.,1 capsule)"
                 InputProps={{
-                  startAdornment: <Inventory sx={{ color: "#20ADA0" }} />,
+                  startAdornment: (
+                    <Inventory sx={{ color: "#20ADA0", mr: 2 }} />
+                  ),
                 }}
               />
             </Grid>
@@ -235,9 +274,11 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                 margin="dense"
                 value={formData.frequency}
                 onChange={handleChange}
-                placeholder="Enter frequency"
+                error={!!errors.frequency}
+                helperText={errors.frequency}
+                placeholder="Enter frequency(e.g.,twice a day)"
                 InputProps={{
-                  startAdornment: <Repeat sx={{ color: "#20ADA0" }} />,
+                  startAdornment: <Repeat sx={{ color: "#20ADA0", mr: 2 }} />,
                 }}
               />
             </Grid>
@@ -249,19 +290,15 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                 margin="dense"
                 value={formData.duration}
                 onChange={handleChange}
-                placeholder="Enter duration"
+                error={!!errors.duration}
+                helperText={errors.duration}
+                placeholder="Enter duration(e.g.,7 days)"
                 InputProps={{
-                  startAdornment: <Timer sx={{ color: "#20ADA0" }} />,
+                  startAdornment: <Timer sx={{ color: "#20ADA0", mr: 2 }} />,
                 }}
               />
             </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={4}
-              sx={{ marginTop: 2 }} 
-              
-            >
+            <Grid item xs={12} sm={4} sx={{ mt: 2 }} className="mt-4">
               <FormControlLabel
                 control={
                   <Checkbox
@@ -269,15 +306,13 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                     onChange={handleCheckboxChange}
                     sx={{
                       color: formData.isEmptyStomach ? "#20ADA0" : "default",
-                      
                       "&.Mui-checked": {
                         color: "#20ADA0",
-                        
                       },
                     }}
                   />
                 }
-                label="Empty Stomach"
+                label="Take on Empty Stomach"
               />
             </Grid>
 
@@ -298,10 +333,17 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                 }}
                 fullWidth
                 renderInput={(params) => (
-                  <TextField {...params} label="Type" variant="outlined" />
+                  <TextField
+                    {...params}
+                    label="Type"
+                    variant="outlined"
+                    error={!!errors.type}
+                    helperText={errors.type || ""}
+                  />
                 )}
               />
             </Grid>
+
             <Grid item xs={12} sm={4}>
               <StyledAutocomplete
                 options={optionsStatus}
@@ -322,6 +364,76 @@ const CreateTreatmentDialog: React.FC<CreateTreatmentDialogProps> = ({
                   <TextField {...params} label="Status" variant="outlined" />
                 )}
               />
+            </Grid>
+
+            {/* Image Upload Grid */}
+            <Grid item xs={12} sm={4}>
+              <Button
+                component="label"
+                variant="contained"
+                startIcon={<ImageIcon />}
+                sx={{
+                  borderRadius: "12px",
+                  padding: "10px",
+                  textAlign: "center",
+                  backgroundColor: "#20ADA0",
+                  cursor: "pointer",
+                  width: "100%",
+                  marginTop: 0.6,
+                  "&:hover": { backgroundColor: "#20ADA0" },
+                  transition: "0.3s",
+                }}
+              >
+                Upload Image
+                <input
+                  type="file"
+                  hidden
+                  accept="image/*"
+                  id="image-upload"
+                  onChange={handleImageUpload}
+                />
+              </Button>
+
+              {formData.photo && (
+                <Box
+                  sx={{
+                    position: "relative",
+                    width: "100%",
+                    maxHeight: "200px",
+                    marginTop: 2,
+                    borderRadius: 8,
+                    overflow: "hidden",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    src={URL.createObjectURL(formData.photo)}
+                    alt="Uploaded"
+                    style={{
+                      width: "100%",
+                      maxHeight: "200px",
+                      objectFit: "cover",
+                      borderRadius: 8,
+                    }}
+                  />
+
+                  {/* Close Button */}
+                  <IconButton
+                    onClick={() => setFormData({ ...formData, photo: null })}
+                    sx={{
+                      position: "absolute",
+                      top: 4,
+                      right: 8,
+                     
+                      color: "#20ADA0",
+                    }}
+                  >
+                    <Close />
+                  </IconButton>
+                </Box>
+              )}
             </Grid>
           </Grid>
         </DialogContent>
