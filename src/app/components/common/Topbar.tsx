947c954c953c01ba7674c649abd95de0467615b4
Mohd hassan <razahassansaifi2@gmail.com>
2025-04-25 11:31:41 +0530

    logo change

--- src/app/components/common/Topbar.tsx
+++ src/app/components/common/Topbar.tsx
@@ -145,30 +145,19 @@ const Topbar = () => {
       }}
     >
       <Toolbar disableGutters>
-        <AdbIcon
-          sx={{
-            display: { xs: "none", md: "flex", color: "#fff" },
-            mr: 0.1,
-            mb: 0.6,
-          }}
-        />
-        <Typography
-          variant="h6"
-          noWrap
-          component="a"
-          href="/"
-          sx={{
-            mr: 2,
-            display: { xs: "none", md: "flex" },
-            fontFamily: "Poppins",
-            fontWeight: 700,
-            letterSpacing: ".3rem",
-            color: "#fff",
-            textDecoration: "none",
-          }}
-        >
-          {en.topbar.title}
-        </Typography>
+        <a href="/">
+          <img
+            src="/logomain.png" // Replace this with your image URL
+            alt="Logo"
+            style={{
+              display: "flex",
+              width: "auto",
+              height: "auto",
+              maxWidth: "30%",
+              marginRight: "8px", // Adjust the spacing as needed
+            }}
+          />
+        </a>
 
         {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
           <IconButton
